f25e1f19c2108fb0f039dfbe0167abda04257e0d
anncannbann <anncannbann@gmail.com>
2021-03-12 19:29:59 +0530

    new

--- questions/binarytree.py
+++ questions/binarytree.py
@@ -4,3 +4,30 @@
 #       def insert(root,level,val)
 #       def del(val,root)
 
+# topmmost node = root ,below are children
+
+
+class Node:
+    def __init__(self,key):
+        self.left = None
+        self.right = None
+        self.val = key
+    
+    def PrintTree(self):
+        print(self.val)
+        
+        if(self.left!=0):
+            print(root.left.val)
+        if(self.right!=0):
+            print(root.right.val)
+
+
+
+#main
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(7)
+root.right.left = Node(5)
+root.left.right = Node(10)
+root.PrintTree()
