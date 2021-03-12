ee4c5b0e0676c099c8cc2b5c9e00d851b4f4dee6
anncannbann <anncannbann@gmail.com>
2021-03-12 19:47:38 +0530

    new

--- questions/binarytree.py
+++ questions/binarytree.py
@@ -8,26 +8,52 @@
 
 
 class Node:
-    def __init__(self,key):
+    def __init__(self,data):
         self.left = None
         self.right = None
-        self.val = key
+        self.data = data
     
     def PrintTree(self):
-        print(self.val)
-        
-        if(self.left!=0):
-            print(root.left.val)
-        if(self.right!=0):
-            print(root.right.val)
+        # print(self.val)
+        if (not data):
+            return 
+        if self.left:
+                self.left.PrintTree()
+        print(self.data,end=" ")
+        if self.right:
+                self.right.PrintTree()
+        #print(self.data)
+
+    def addNode(self,data):
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.addNode(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.addNode(data)
+        else:
+            self.data = data
 
 
 
 #main
 root = Node(1)
-root.left = Node(2)
-root.right = Node(3)
-root.left.left = Node(7)
-root.right.left = Node(5)
-root.left.right = Node(10)
+root.left = Node(11) 
+root.left.left = Node(7) 
+root.left.right = Node(12) 
+root.right = Node(9) 
+root.right.left = Node(15) 
+root.right.right = Node(8) 
+print("The tree before the deletion:") 
+# root.addNode(2)
+# root.addNode(3)
+# root.addNode(4)
+# root.addNode(5)
+# root.addNode(6)
+
 root.PrintTree()
